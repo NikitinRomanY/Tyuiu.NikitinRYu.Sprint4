00e728c2b1d449484465c0924e5974fa1bd5d0a1
Tyuiu.NikitinRy@outlook.com <Tyuiu.NikitinRy@outlook.com>
2025-11-10 22:09:57 +0500

    Task4

---
+++ Tyuiu.NikitinRYu.Sprint4.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.NikitinRYu.Sprint4.Task4.V18.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint4.Task4.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5]
+            {
+                {4, 7, 4, 5, 4},
+                {4, 4, 5, 6, 7},
+                {7, 5, 6, 6, 6},
+                {6, 7, 6, 6, 4},
+                {5, 6, 6, 6, 5}
+            };
+
+            int[,] wait = new int[5, 5]
+            {
+                {4, 0, 4, 0, 4},
+                {4, 4, 0, 6, 0},
+                {0, 0, 6, 6, 6},
+                {6, 0, 6, 6, 4},
+                {0, 6, 6, 6, 0}
+            };
+
+            int[,] res = ds.Calculate(matrix);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NikitinRYu.Sprint4.Task4.V18/Program.cs
@@ -0,0 +1,69 @@
+﻿using Tyuiu.NikitinRYu.Sprint4.Task4.V18.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint4.Task4.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Никитин Р.Ю. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Никитин Р.Ю. | ИБКСб-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 4 до 7. Заменить нечетные        *");
+            Console.WriteLine("* элементы массива на 0.                                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+
+            Console.WriteLine("Введите элементы массива (5x5, значения от 4 до 7):");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"Элемент [{i},{j}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nИсходный массив:");
+            PrintArray(matrix);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int[,] result = ds.Calculate(matrix);
+
+            Console.WriteLine("Массив после замены нечётных элементов на 0:");
+            PrintArray(result);
+
+            Console.ReadKey();
+        }
+
+        static void PrintArray(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
