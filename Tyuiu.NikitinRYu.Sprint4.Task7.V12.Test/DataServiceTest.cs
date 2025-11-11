0af6d5e2593c69549246dcc23686b6869caf9203
Tyuiu.NikitinRy@outlook.com <Tyuiu.NikitinRy@outlook.com>
2025-11-11 22:24:46 +0500

    Task7

---
+++ Tyuiu.NikitinRYu.Sprint4.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.NikitinRYu.Sprint4.Task7.V12.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint4.Task7.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int n = 3;
+            int m = 4;
+            string value = "658932125478";
+
+            int wait = 30;
+            int res = ds.Calculate(n, m, value);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NikitinRYu.Sprint4.Task7.V12/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.NikitinRYu.Sprint4.Task7.V12.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint4.Task7.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Никитин Р.Ю. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Никитин Р.Ю. | ИБКСб-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '658932125478'. Преобразуйте ее в     *");
+            Console.WriteLine("* матрицу 3 на 4 и подсчитайте сумму нечетных чисел.                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int n = 3;
+            int m = 4;
+            string value = "658932125478";
+
+            Console.WriteLine($"Количество строк (n) = {n}");
+            Console.WriteLine($"Количество столбцов (m) = {m}");
+            Console.WriteLine($"Исходная строка = {value}");
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int result = ds.Calculate(n, m, value);
+
+            Console.WriteLine($"Сумма нечётных чисел = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
